3989b53432e28990aa4cc59ded129106645cea7f
김수진 <snsk0612@betree.co.kr>
2025-03-24 16:10:23 +0900

    넉스트 플러그인

--- app.vue
+++ app.vue
@@ -4,5 +4,9 @@
   </NuxtLayout>
 </template>
 <script setup lang="ts">
+  const nuxtApp = useNuxtApp();
+  console.log('nuxtApp: ', nuxtApp);
 
+  //provide 활용
+  // nuxtApp.provide('hello', (msg: string) => `Hello ${msg}`);
 </script>
